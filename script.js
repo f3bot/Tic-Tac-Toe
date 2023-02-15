e92e83791e0988efcbfef801b76760dfc2cd021a
f3bot <f3bot@protonmail.com>
2023-02-15 17:55:35 +0100

    Update 04 -> Added logic that checks if the game's over

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const main = document.querySelector('.main')
 const gameBoard = (() =>{
     let playerOneTurn = true
     const array = []
+    let isGameOverBool = false
     const renderBoard = () =>{
         for(let i = 0; i < 3; i++)
         {
@@ -21,15 +22,61 @@ const gameBoard = (() =>{
         console.log(array)
         return {array}
     }
+
+    const checkForWinner = () =>{
+        //vertically
+        for(let i = 0; i < 9; i+=3)
+        {
+            if(array[i].textContent == array[i+1].textContent && array[i+1].textContent == array[i+2].textContent && array[i].textContent != '')
+            {
+                isGameOverBool = true
+            }
+        }
+        //horizontally
+        for(let i = 0; i < 3; i++)
+        {
+            if(array[i].textContent == array[i+3].textContent && array[i+3].textContent == array[i+6].textContent && array[i].textContent != '')
+            {
+                isGameOverBool = true
+            }
+        }
+        //diagonally
+        if(array[0].textContent == array[4].textContent && array[4].textContent == array[8].textContent && array[0].textContent != '')
+        {
+            isGameOverBool = true
+        }
+        
+        if(array[2].textContent == array[4].textContent && array[4].textContent == array[6].textContent && array[2].textContent != '')
+        {
+            isGameOverBool = true
+        }
+
+        if(array.every(element => element.textContent !== ''))
+        {
+            isGameOverBool = true
+        }
+        
+    }
+
+
+    const isGameEnd = () =>{
+        checkForWinner()
+        if(isGameOverBool){
+            console.log("Game Over!")
+        }
+    }
+
     const addListeners = (fun) =>{
         array.forEach(element => element.addEventListener('click', (e) =>{
             console.log('clicked' + array.indexOf(element))
             if(playerOneTurn) {
                 playerX.markSpot(element)
                 playerOneTurn = false
+                isGameEnd()
             }else{
                 playerO.markSpot(element)
                 playerOneTurn = true
+                isGameEnd()
             }
         }))
         return array
@@ -44,28 +91,15 @@ const gameBoard = (() =>{
 
 const player = (name) =>{
     const markSpot = (item) =>{
-        if(item.innerHTML == '')
+        if(item.textContent == '')
         {
-            item.innerHTML = name
+            item.textContent = name
         }
         else
         {
             console.log("DAJ TU WALIDACJE")
         }
-        checkForWinner()
         return {}
-    }
-    const checkForWinner = () =>{
-        const winningCombinations = [
-        [0,1,2],
-         [3,4,5],
-          [6,7,8],
-           [0,4,8],
-            [2,4,6],
-             [0,3,6],
-              [1,4,7],
-               [2,5,8]]
-    console.log("GAME ENDED")
     }
     return {markSpot}
 }
